315632e1f056df5c7083fd94dfb7fa2a5be7fcd9
adityapandey27 <adityapandey272002@gmail.com>
2025-09-05 01:52:49 +0530

    basic test

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test("First React app case",()=>{
+  render (<App/>);
+  const text=screen.getByText(/First React Test Case/);
+  expect(text).toBeInTheDocument();
+
+})
 
 
 
