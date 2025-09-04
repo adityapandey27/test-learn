050c99a5e21b308bd724d3eeb18488515e11510f
adityapandey27 <adityapandey272002@gmail.com>
2025-09-05 02:40:12 +0530

    tested the input filed with all its attribute

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <p>First React Test Case</p>
-      <img title='image from pixabay' src='https://cdn.pixabay.com/photo/2023/07/19/12/16/car-8136751_1280.jpg'/>
+      <br />
+      <br />
+      <input
+        type="text"
+        placeholder="Enter Your Name"
+        name="username"
+        id="userId"
+        value={"aditya"}
+      />
+      <br />
+      <br />
+      <img
+        title="image from pixabay"
+        src="https://cdn.pixabay.com/photo/2023/07/19/12/16/car-8136751_1280.jpg"
+      />
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,28 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
 
-test("First React app case",()=>{
-  render (<App/>);
-  const text=screen.getByText(/First React Test Case/);
+import App from "./App";
+
+test("First React app case", () => {
+  render(<App />);
+  const text = screen.getByText(/First React Test Case/);
   // expect is part of jest
-  const title=screen.getByTitle("image from pixabay")
+
+  const title = screen.getByTitle("image from pixabay");
   expect(text).toBeInTheDocument();
   expect(title).toBeInTheDocument();
+});
+test("Testing input filed", () => {
+  render(<App />);
+  const chekcInput = screen.getByRole("textbox");
+  // testing place holder
+  let checkPlaceHolder = screen.getByPlaceholderText("Enter Your Name");
 
-})
-
-
-
-
+  expect(checkPlaceHolder).toBeInTheDocument();
+  expect(chekcInput).toBeInTheDocument();
+  expect(chekcInput).toHaveAttribute("name", "username");
+  expect(chekcInput).toHaveAttribute("id", "userId");
+  expect(chekcInput).toHaveAttribute("type", "text");
+  //  expect(chekcInput).toHaveAttribute("value","aditya")
+  // this one will give error:" console.error You provided a `value` prop to a form field without an `onChange` handler. This will render a read-only field. If the field should be mutable use `defaultValue`. Otherwise, set either `onChange` or `readOnly`."
+  // occurs when we try to provide static value
+});
