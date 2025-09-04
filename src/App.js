deb63869c2c723022b1c50228299bcce30252ca7
adityapandey27 <adityapandey272002@gmail.com>
2025-09-05 02:18:29 +0530

    vid5: img and title with it

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <p>First React Test Case</p>
+      <img title='image from pixabay' src='https://cdn.pixabay.com/photo/2023/07/19/12/16/car-8136751_1280.jpg'/>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -4,7 +4,10 @@ import App from './App';
 test("First React app case",()=>{
   render (<App/>);
   const text=screen.getByText(/First React Test Case/);
+  // expect is part of jest
+  const title=screen.getByTitle("image from pixabay")
   expect(text).toBeInTheDocument();
+  expect(title).toBeInTheDocument();
 
 })
 
